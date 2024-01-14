e4ca5ef4bb38a8c8244696a50d268ae62daf53fd
Renan Cesar <cesar.renan@hotmail.com>
2024-01-13 22:48:57 -0300

    feat: botão de adicionar talento

--- src/components/features/Tabs/index.tsx
+++ src/components/features/Tabs/index.tsx
@@ -22,112 +22,11 @@ import { Search } from '@mui/icons-material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import { DataGrid } from '@mui/x-data-grid';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import elementos from '../../../utils/elementos.json';
 import CustomButton from '../../ui/Button';
-
-const columns = [
-    {
-        field: 'img',
-        headerName: '',
-        width: 60,
-        sortable: false,
-        filterable: false,
-        hideable: false,
-        renderCell: (params: any) => {
-            return (
-                <Box
-                    sx={{
-                        display: "flex",
-                        gap: "10px",
-
-                        "& img": {
-                            width: 40,
-                            height: 40,
-                            borderRadius: 50
-                        }
-                    }}
-                >
-                    {
-                        params.row.img ?
-                            <img src={params.row.img} alt="foto-perfil" />
-                            :
-                            <Avatar />
-                    }
-                </Box>
-            )
-        }
-    },
-    {
-        field: 'id',
-        headerName: 'ID',
-        width: 60
-    },
-    {
-        field: 'nome',
-        headerName: 'Nome',
-        minWidth: 180,
-        flex: 1
-    },
-    {
-        field: 'email',
-        headerName: 'Email',
-        minWidth: 230,
-        flex: 1
-    },
-    {
-        field: 'trilha',
-        headerName: 'Trilhas',
-        minWidth: 90,
-        maxWidth: 200,
-        flex: 1,
-    },
-    {
-        field: 'status',
-        headerName: 'Status',
-        width: 140,
-        renderCell: (params: any) => {
-            return (
-                <Typography
-                    variant="body2"
-                    color={params.value === 'T' ? 'text.primary' : 'red'}
-                >
-                    {params.value === 'T' ? 'disponivel' : 'Indisponivel'}
-                </Typography>
-            )
-        }
-    },
-    {
-        field: 'acoes',
-        headerName: 'Ações',
-        width: 120,
-        sortable: false,
-        filterable: false,
-        hideable: false,
-        renderCell: (params: any) => {
-            return (
-                <Box
-                    sx={{
-                        display: "flex",
-                        gap: "10px"
-                    }}
-                >
-                    <Tooltip title="Editar">
-                        <Button variant="contained" id="edit" sx={{ width: '40px', minWidth: "40px", opacity: 0.7, "&:hover": { opacity: 1 } }} onClick={() => console.log(params.row)}>
-                            <EditNoteIcon />
-                        </Button>
-                    </Tooltip>
-                    <Tooltip title="Deletar">
-                        <Button variant="contained" id="delete" sx={{ background: "red", width: '40px', minWidth: "40px", opacity: 0.7, "&:hover": { opacity: 1, background: "red" } }} onClick={() => console.log(params.row.id)}>
-                            <DeleteIcon />
-                        </Button>
-                    </Tooltip>
-                </Box>
-            )
-        },
-    }
-];
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const trilhaArr = [
     { nome: "Frontend" },
@@ -138,13 +37,6 @@ const trilhaArr = [
     { nome: "UI/UX Design" },
     { nome: "Mobile Development" },
     { nome: "QA" }
-];
-
-const edicoesArr = [
-    { nome: "1ª" },
-    { nome: "2ª" },
-    { nome: "3ª" },
-    { nome: "4ª" },
 ];
 
 const rows = () => {
@@ -173,7 +65,6 @@ export const Tabs = () => {
     const [isLoading, setisLoading] = useState(false);
     const [email, setEmail] = useState('');
     const [emailInput, setEmailInput] = useState('');
-    const [edicao, setEdicao] = useState('');
     const [trilha, setTrilha] = useState('');
 
     const theme = useTheme();
@@ -182,10 +73,111 @@ export const Tabs = () => {
     const resetFiltro = () => {
         setEmail('');
         setEmailInput('');
-        setEdicao('');
         setTrilha('');
         setPage(0);
     };
+
+    const columns = [
+        {
+            field: 'img',
+            headerName: '',
+            width: 60,
+            sortable: false,
+            filterable: false,
+            hideable: false,
+            renderCell: (params: any) => {
+                return (
+                    <Box
+                        sx={{
+                            display: "flex",
+                            gap: "10px",
+
+                            "& img": {
+                                width: 40,
+                                height: 40,
+                                borderRadius: 50
+                            }
+                        }}
+                    >
+                        {
+                            params.row.img ?
+                                <img src={params.row.img} alt="foto-perfil" />
+                                :
+                                <Avatar />
+                        }
+                    </Box>
+                )
+            }
+        },
+        {
+            field: 'id',
+            headerName: 'ID',
+            width: 60
+        },
+        {
+            field: 'nome',
+            headerName: 'Nome',
+            minWidth: 180,
+            flex: 1
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            minWidth: 230,
+            flex: 1
+        },
+        {
+            field: 'trilha',
+            headerName: 'Trilhas',
+            minWidth: 90,
+            maxWidth: 200,
+            flex: 1,
+        },
+        {
+            field: 'status',
+            headerName: 'Status',
+            width: 140,
+            renderCell: (params: any) => {
+                return (
+                    <Typography
+                        variant="body2"
+                        color={params.value === 'T' ? 'text.primary' : 'red'}
+                    >
+                        {params.value === 'T' ? 'disponivel' : 'Indisponivel'}
+                    </Typography>
+                )
+            }
+        },
+        {
+            field: 'acoes',
+            headerName: 'Ações',
+            width: 120,
+            sortable: false,
+            filterable: false,
+            hideable: false,
+            renderCell: (params: any) => {
+                return (
+                    <Box
+                        sx={{
+                            display: "flex",
+                            gap: "10px"
+                        }}
+                    >
+                        <Tooltip title="Editar">
+                            <Button variant="contained" id="edit" sx={{ width: '40px', minWidth: "40px", opacity: 0.7, "&:hover": { opacity: 1 } }} onClick={() => { console.log(params.row); navigate('/talento/editar', { state: params.row }) }}>
+                                <EditNoteIcon />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip title="Deletar">
+                            <Button variant="contained" id="delete" sx={{ background: "red", width: '40px', minWidth: "40px", opacity: 0.7, "&:hover": { opacity: 1, background: "red" } }} onClick={() => console.log(params.row.id)}>
+                                <DeleteIcon />
+                            </Button>
+                        </Tooltip>
+                    </Box>
+                )
+            },
+        }
+    ];
 
     return (
         <Grid container spacing={2} sx={{ paddingInline: "20px", marginBlock: "20px" }}>
@@ -241,40 +233,42 @@ export const Tabs = () => {
                             ;
                         </Select>
                     </FormControl>
-                    <FormControl fullWidth size="small">
-                        <InputLabel>Filtrar por edição</InputLabel>
-                        <Select
-                            label="Filtrar por edição"
-                            id="registros-filter-by-edition"
-                            defaultValue=""
-                            value={edicao}
-                            onChange={e => {
-                                setEdicao(e.target.value)
-                            }}
-                        >
-                            {edicoesArr?.map((edicao: any) => {
-                                return (
-                                    <MenuItem
-                                        key={edicao.nome}
-                                        value={edicao.nome}
-                                        id={`filtro-edicao-${edicao.nome}`}
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            gap: '15px'
+                        }}
+                    >
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <CustomButton
+                                color={"var(--font-color-primary)"}
+                                backgroundcolor={"var(--bg-section-claro)"}
+                                backgroundcolorhover={"var(--bg-section-escuro)"}
+                                onClickButton={resetFiltro}
+                            >
+                                Limpar
+                            </CustomButton>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <Link to='/adicionar'>
+                                <CustomButton
+                                    color={"var(--font-color-primary)"}
+                                    backgroundcolor={"var(--bg-section-claro)"}
+                                    backgroundcolorhover={"var(--bg-section-escuro)"}
+                                >
+                                    <Box
+                                        sx={{
+                                            gap: '10px',
+                                            display: 'flex'
+                                        }}
                                     >
-                                        {edicao.nome}
-                                    </MenuItem>
-                                )
-                            })}
-                            ;
-                        </Select>
-                    </FormControl>
-                    <Box display="flex" alignItems="center" justifyContent="center">
-                        <CustomButton
-                            color={"var(--font-color-primary)"}
-                            backgroundcolor={"var(--bg-section-claro)"}
-                            backgroundcolorhover={"var(--bg-section-escuro)"}
-                            onClickButton={resetFiltro}
-                        >
-                            Limpar
-                        </CustomButton>
+                                        <PersonAddIcon />
+                                        Adicionar
+                                    </Box>
+                                </CustomButton>
+                            </Link>
+                        </Box>
                     </Box>
                 </Stack>
             </Grid>
